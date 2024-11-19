ceed5afe433ed58e0071e9b0f3449074555372f9
Yoos99 <yoosh2520@naver.com>
2024-11-19 15:31:13 +0900

    feat: 레이아웃 분리 및 BottomNavBar 조건부 렌더링 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BottomNavBar } from './components';
+import MainLayout from './layouts/MainLayout';
+import AuthLayout from './layouts/AuthLayout';
 import { GamePage, MainPage, MyPage, SearchPage } from './pages';
 import CreateGame from './pages/game/CreateGame';
 import WaitingRoom from './pages/game/WaitingRoom';
+import CallbackPage from './pages/Login/CallbackPage';
+import LoginPage from './pages/Login/LoginPage';
 import { useState } from 'react';
 
 function App() {
@@ -10,18 +13,24 @@ function App() {
 
   return (
     <Router>
-      <div className="pb-16">
-        <Routes>
+      <Routes>
+        {/* 인증 관련 경로는 AuthLayout을 사용 */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/auth/kakao/callback" element={<CallbackPage provider="kakao" />} />
+          <Route path="/auth/google/callback" element={<CallbackPage provider="google" />} />
+        </Route>
+
+        {/* 나머지 경로는 MainLayout을 사용 */}
+        <Route element={<MainLayout activeTab={activeTab} setActiveTab={setActiveTab} />}>
           <Route path="/" element={<MainPage />} />
           <Route path="/game" element={<GamePage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/profile" element={<MyPage />} />
           <Route path="/game/create" element={<CreateGame />} />
           <Route path="/game/waiting" element={<WaitingRoom />} />
-          {/* 추가적인 페이지 라우팅을 등록 */}
-        </Routes>
-        <BottomNavBar activeTab={activeTab} setActiveTab={setActiveTab} />
-      </div>
+        </Route>
+      </Routes>
     </Router>
   );
 }
