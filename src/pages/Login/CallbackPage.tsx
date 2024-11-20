fb422058ac57c88e1754eb2098f86a0b49e98a75
Yoos99 <yoosh2520@naver.com>
2024-11-20 16:50:05 +0900

    style: 주석 제거 및 폴더 이름 통일

--- src/pages/Login/CallbackPage.tsx
+++ src/pages/Login/CallbackPage.tsx
@@ -2,7 +2,7 @@ import useOAuthHandler from '@/hooks/useOAuthHandler';
 
 const CallbackPage = ({ provider }: { provider: 'google' | 'kakao' }) => {
   useOAuthHandler(provider);
-  console.log('로그인 처리중...');
+
   return <div>로그인 처리 중...</div>;
 };
 
