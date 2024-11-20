538e6c4fa7a3cb5eff601d68a1e888473bfffe9c
Wonchang0314 <andrew0314@naver.com>
2024-11-20 16:33:52 +0900

    FIX: 브랜드 아이콘 설정 & TopBar 컴포넌트 일부 수정

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,7 +1,13 @@
+import { useReducer } from 'react';
 import Avatar from '@eolluga/eolluga-ui/Display/Avatar';
 import Icon from '@eolluga/eolluga-ui/icon/Icon';
-import logoImage from '../../assests/logoImage.svg';
+import { MdLogout } from 'react-icons/md';
+import { SiQuizlet } from 'react-icons/si';
+import { Popover, PopoverContent, PopoverTrigger } from '@/shadcn/ui/popover';
+import { Button as ShadcnButton } from '@/shadcn/ui/button';
+import Dialog from './Dialog';
 import defaultImageURL from '@/shared/defaultImage';
+import FlexBox from '@/shared/FlexBox';
 
 type TopBarProps = {
   title?: string;
@@ -10,31 +16,116 @@ type TopBarProps = {
   onClickLeft?: () => void;
 };
 
+type ActionType =
+  | { type: 'TOGGLE_POP' }
+  | { type: 'LOGOUT' }
+  | { type: 'EXIT' }
+  | { type: 'CLOSE' };
+
+type StateType = {
+  isPop: boolean;
+  showLogoutDialog: boolean;
+  showExitDialog: boolean;
+};
+
+const reducer = (state: StateType, action: ActionType): StateType => {
+  switch (action.type) {
+    case 'TOGGLE_POP':
+      return { ...state, isPop: !state.isPop };
+    case 'LOGOUT':
+      return { ...state, isPop: false, showLogoutDialog: true };
+    case 'EXIT':
+      return { ...state, isPop: false, showExitDialog: true };
+    case 'CLOSE':
+      return { ...state, isPop: false, showLogoutDialog: false, showExitDialog: false };
+    default:
+      return state;
+  }
+};
+
+const initialState: StateType = {
+  isPop: false,
+  showLogoutDialog: false,
+  showExitDialog: false,
+};
+
 const TopBar = ({ leftIcon = 'default', leftText = '', onClickLeft }: TopBarProps) => {
-  const checkUserProfile = () => {
-    console.log('다른 사용자 프로필 조회');
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // 로그아웃
+  const handleLogout = () => {
+    dispatch({ type: 'LOGOUT' });
+  };
+
+  const handleExit = () => {
+    dispatch({ type: 'EXIT' });
   };
 
   return (
     <header className="bg-white border-b fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
-        <div className="flex gap-4 items-center">
+        <div className={`flex ${leftIcon === 'default' ? 'gap-1' : 'gap-4'} items-center`}>
           {leftIcon === 'default' ? (
-            <img src={logoImage} alt="logo" />
+            <SiQuizlet size={32} color="blue" />
           ) : (
-            <div onClick={onClickLeft}>
+            <div className="cursor-pointer" onClick={onClickLeft}>
               <Icon icon={'chevron_left_outlined'} />
             </div>
           )}
           {leftText === '' ? (
-            <span className="text-xl font-bold">Quizy</span>
+            <span className="text-2xl font-bold">uizy</span>
           ) : (
             <span className="text-xl font-bold">{leftText}</span>
           )}
         </div>
-        <div onClick={checkUserProfile} className="cursor-pointer">
-          <Avatar input="image" image={defaultImageURL} size="S" />
-        </div>
+        <Popover open={state.isPop} onOpenChange={() => dispatch({ type: 'TOGGLE_POP' })}>
+          <PopoverTrigger asChild>
+            <button className="cursor-pointer">
+              <Avatar input="image" image={defaultImageURL} size="S" />
+            </button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <p className="w-full py-4 text-lg text-center">안녕하세요 원석님</p>
+            <FlexBox direction="col" className="gap-2">
+              <ShadcnButton
+                variant="ghost"
+                className="w-full py-4 font-semibold text-lg border-b"
+                onClick={handleLogout}
+              >
+                <MdLogout size={32} />
+                로그아웃
+              </ShadcnButton>
+              <ShadcnButton
+                variant="ghost"
+                className="w-full py-4 font-semibold text-lg text-red-400"
+                onClick={handleExit}
+              >
+                <Icon icon="close_circle" size={32} />
+                탈퇴하기
+              </ShadcnButton>
+            </FlexBox>
+          </PopoverContent>
+        </Popover>
+        <Dialog
+          open={state.showLogoutDialog}
+          onClose={() => dispatch({ type: 'CLOSE' })}
+          title="로그아웃"
+          description="정말 로그아웃 하시겠습니까?"
+          leftText="예"
+          rightText="아니요"
+          leftOnClick={() => dispatch({ type: 'CLOSE' })}
+          rightOnClick={() => dispatch({ type: 'CLOSE' })}
+        />
+        <Dialog
+          open={state.showExitDialog}
+          onClose={() => dispatch({ type: 'CLOSE' })}
+          title="회원 탈퇴"
+          description="이 작업은 되돌릴 수 없으며, 모든 데이터가 영구적으로 삭제됩니다."
+          leftText="예"
+          rightText="아니요"
+          leftOnClick={() => dispatch({ type: 'CLOSE' })}
+          rightOnClick={() => dispatch({ type: 'CLOSE' })}
+        />
       </div>
     </header>
   );
