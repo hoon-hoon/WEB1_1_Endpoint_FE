0335e54faca58e2782e56eab946435533124ed86
Yoos99 <yoosh2520@naver.com>
2024-11-21 15:29:03 +0900

    style: 소셜 로그인 높이 수정

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@/shadcn/ui/button';
-import Icon from '@eolluga/eolluga-ui/icon/Icon';
+import { Button } from "@/shadcn/ui/button";
+import Icon from "@eolluga/eolluga-ui/icon/Icon";
 
 const GoogleLoginButton = () => {
   const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
@@ -11,14 +11,23 @@ const GoogleLoginButton = () => {
   };
 
   return (
+    // <Button
+    //   variant="google"
+    //   size="lg"
+    //   className="w-full flex items-center justify-center gap-2 text-lg"
+    //   onClick={handleGoogleLogin}
+    // >
+    //   <Icon icon="google_color" size={36} />
+    //   <span className="text-base font-semibold">구글로 시작하기</span>
+    // </Button>
     <Button
       variant="google"
       size="lg"
-      className="w-full flex items-center justify-center gap-2 text-lg"
+      className="w-full flex items-center justify-center gap-2 text-xl py-6"
       onClick={handleGoogleLogin}
     >
-      <Icon icon="google_color" size={36} />
-      <span className="text-base font-semibold">구글로 시작하기</span>
+      <Icon icon="google_color" size={48} />
+      <span className="text-[17px] font-semibold">구글로 시작하기</span>
     </Button>
   );
 };
