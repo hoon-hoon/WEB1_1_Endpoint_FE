e8bbb76774487983c0607d19fb5d52e4f925c840
hoon-hoon <kc0393@naver.com>
2024-11-28 11:59:16 +0900

    feat: 아이콘 교체 및 스타일 수정

--- src/components/Quiz/QuizFooter.tsx
+++ src/components/Quiz/QuizFooter.tsx
@@ -1,4 +1,5 @@
-import Icon from '@eolluga/eolluga-ui/icon/Icon';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { FaRegComment } from 'react-icons/fa';
 
 interface QuizFooterProps {
   likes: number;
@@ -10,14 +11,24 @@ interface QuizFooterProps {
 
 function QuizFooter({ likes, comments, isLiked, onToggleLike, onCommentsClick }: QuizFooterProps) {
   return (
-    <div className="flex items-center mt-4 text-gray-600">
-      <button className="flex items-center mr-6" onClick={onToggleLike} aria-label="좋아요">
-        <Icon icon={isLiked ? 'heart_filled' : 'heart_outlined'} />
+    <div className="flex mt-4 text-gray-600">
+      <button
+        className="flex items-center p-2  mr-1 rounded-full hover:bg-gray-100 active:bg-gray-200"
+        onClick={onToggleLike}
+      >
+        {isLiked ? (
+          <AiFillHeart className="w-6 h-6 text-red-500" />
+        ) : (
+          <AiOutlineHeart className="w-6 h-6" />
+        )}
         <span className="ml-1">{likes}</span>
       </button>
 
-      <button className="flex items-center" onClick={onCommentsClick} aria-label="댓글">
-        <Icon icon="chat" />
+      <button
+        className="flex items-center p-2 rounded-full hover:bg-gray-100 active:bg-gray-200"
+        onClick={onCommentsClick}
+      >
+        <FaRegComment className="w-6 h-6" />
         <span className="ml-1">{comments}</span>
       </button>
     </div>
