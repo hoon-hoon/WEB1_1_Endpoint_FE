c1a285186ebc64cd156c94cfe097214b854814db
Wonchang0314 <andrew0314@naver.com>
2024-11-29 12:02:08 +0900

    FIX: 로그인 잘못된 예외처리 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const axiosInstance = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: BACKEND_URL + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
