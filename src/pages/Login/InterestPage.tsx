4505607a1cc9978aa353a73416ff7a9f07eb0286
Yoos99 <yoosh2520@naver.com>
2024-12-04 16:55:48 +0900

    feat: 관심사 api 연동 및 관심사 수정 - 서버와 단어 맞춤

--- src/pages/Login/InterestPage.tsx
+++ src/pages/Login/InterestPage.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button as ShadcnButton } from '@/shadcn/ui/button';
 import InterestButton from '../../components/common/Button/InterestButton';
+import useRegisterInterests from '@/api/login/useRegisterInterests';
+import { toEnglishCategory } from '@/utils/categoryConverter';
 
-const mock_interests = [
+const interests = [
   '알고리즘',
   '프로그래밍 언어',
   '네트워크',
@@ -15,6 +18,7 @@ const mock_interests = [
 ];
 
 function InterestPage() {
+  const navigate = useNavigate();
   const [selectedInterests, setSelectedInterests] = useState<string[]>([]);
 
   const handleInterestSelect = (interest: string) => {
@@ -23,6 +27,21 @@ function InterestPage() {
         ? prevSelected.filter((item) => item !== interest)
         : [...prevSelected, interest],
     );
+  };
+
+  const { mutate: registerInterests } = useRegisterInterests(
+    () => {
+      console.log('관심사가 성공적으로 등록되었습니다.');
+    },
+    (error) => {
+      console.error(error);
+    },
+  );
+
+  const handleSubmit = () => {
+    const englishInterests = selectedInterests.map(toEnglishCategory);
+    registerInterests({ interests: englishInterests });
+    navigate('/main');
   };
 
   return (
@@ -34,7 +53,7 @@ function InterestPage() {
         </p>
 
         <div className="grid grid-cols-2 gap-4 mb-8">
-          {mock_interests.map((interest) => (
+          {interests.map((interest) => (
             <InterestButton
               key={interest}
               label={interest}
@@ -50,7 +69,8 @@ function InterestPage() {
           variant="default"
           size="lg"
           className="w-full h-12 text-base"
-          onClick={() => console.log('선택된 관심사:', selectedInterests)}
+          onClick={handleSubmit}
+          disabled={selectedInterests.length === 0} // 로딩 중 또는 선택된 관심사가 없을 때 버튼 비활성화
         >
           선택 완료
         </ShadcnButton>
