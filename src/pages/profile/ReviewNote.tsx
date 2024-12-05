dde20697a2a9d113099ce0e5ef68d7babdf36e87
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-12-05 11:34:52 +0900

    feat: 오답노트 로딩부분 수정

--- src/pages/profile/ReviewNote.tsx
+++ src/pages/profile/ReviewNote.tsx
@@ -6,6 +6,7 @@ import FlexBox from '@/components/layout/FlexBox';
 import axiosInstance from '@/api/axiosInstance';
 import { useQuery } from '@tanstack/react-query';
 import { QuizWithAnswer } from '@/types/WrongQuizTypes';
+import { Loader2 } from 'lucide-react';
 
 const fetchIncorrectAnswers = async () => {
   const response = await axiosInstance.get<{ result: { content: QuizWithAnswer[] } }>(
@@ -27,26 +28,24 @@ export default function ReviewNote() {
     queryFn: fetchIncorrectAnswers,
   });
 
-  if (isPending) {
-    return <div>로딩 중...</div>;
-  }
-
-  if (isError) {
-    return <div>오류가 발생했습니다: {error.message}</div>;
-  }
-
   return (
     <Container>
       <TopBar leftIcon="left" leftText="오답노트" onClickLeft={() => navigate('/profile')} />
-      {incorrectQuizzes.length === 0 ? (
+      {isPending ? (
+        <div className="flex justify-center items-center h-[calc(100vh-100px)]">
+          <Loader2 className="h-16 w-16 text-blue-500 animate-spin" />
+        </div>
+      ) : isError ? (
+        <div>오류가 발생했습니다: {error.message}</div>
+      ) : incorrectQuizzes.length === 0 ? (
         <div className="flex justify-center items-center h-[calc(100vh-100px)] text-gray-500">
           틀린 문제가 없습니다
         </div>
       ) : (
         <FlexBox direction="col" className="gap-4">
           {incorrectQuizzes.map((quizData) => (
-            <div className="w-full">
-              <ReviewNoteCard key={quizData.quiz.id} quizData={quizData} />
+            <div key={quizData.quiz.id} className="w-full">
+              <ReviewNoteCard quizData={quizData} />
             </div>
           ))}
         </FlexBox>
