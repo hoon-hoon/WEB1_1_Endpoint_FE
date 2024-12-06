6e6ac5d93ffcf2855e09cf5b3772cc5c33761f5d
hoon-hoon <kc0393@naver.com>
2024-12-06 17:02:28 +0900

    fix: 배포 오류 수정 vite 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), visualizer()],
-  define: {
-    global: 'window',
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
