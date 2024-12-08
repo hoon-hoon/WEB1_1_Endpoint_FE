07ed05aadd4dc2c268ccf4e69952d70cf3c6e014
hoon-hoon <kc0393@naver.com>
2024-12-08 14:05:59 +0900

    feat: 피드 로딩시 스피너 및 메시지

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -6,6 +6,7 @@ import TopBar from '@/components/common/TopBar';
 import Container from '@/components/layout/Container';
 import QuizCard from './QuizCard';
 import useQuizFeed from '@/api/main/fetchQuiz';
+import Spinner from '@/components/common/Spinner';
 
 const MainPage = () => {
   const { data, fetchNextPage, hasNextPage, isFetching, isError } = useQuizFeed();
@@ -16,22 +17,36 @@ const MainPage = () => {
     <>
       <TopBar />
       <Container>
-        <Swiper
-          direction="vertical"
-          className="quizSwiper h-[80dvh]"
-          spaceBetween={40}
-          onReachEnd={() => {
-            if (hasNextPage) fetchNextPage();
-          }}
-        >
-          {quizzes.map((quiz) => (
-            <SwiperSlide key={quiz.id}>
-              <QuizCard quiz={quiz} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        {isFetching && <div>로딩 중..</div>}
-        {isError && <div>데이터 불러오는 중..</div>}
+        {isFetching ? (
+          <div className="flex flex-col items-center justify-center h-[80dvh] space-y-4">
+            <Spinner />
+            <p className="text-gray-600">퀴즈를 불러오고 있습니다...</p>
+          </div>
+        ) : quizzes.length === 0 ? (
+          <div className="flex items-center justify-center h-[80dvh] text-gray-600">
+            퀴즈가 없습니다.
+          </div>
+        ) : (
+          <Swiper
+            direction="vertical"
+            className="quizSwiper h-[80dvh]"
+            spaceBetween={40}
+            onReachEnd={() => {
+              if (hasNextPage) fetchNextPage();
+            }}
+          >
+            {quizzes.map((quiz) => (
+              <SwiperSlide key={quiz.id}>
+                <QuizCard quiz={quiz} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+        {isError && (
+          <div className="flex items-center justify-center h-[80dvh] text-red-500">
+            데이터를 불러오는 중 오류가 발생했습니다.
+          </div>
+        )}
       </Container>
     </>
   );
