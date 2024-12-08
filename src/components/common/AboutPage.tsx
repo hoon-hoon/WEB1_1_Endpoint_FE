5e3e38ebf3281d7e3c3e3f0358d106fbc62dc76f
Wonchang0314 <andrew0314@naver.com>
2024-12-08 12:11:46 +0900

    FEAT: 동적태그 생성 컴포넌트 추가 및 axiosInstance 코드 일부 수정

--- src/components/common/AboutPage.tsx
+++ src/components/common/AboutPage.tsx
@@ -3,12 +3,15 @@ import { Helmet } from 'react-helmet';
 interface AboutPageProps {
   title: string;
   description: string;
+  keywords: string;
 }
-const AboutPage = ({ title, description }: AboutPageProps) => {
+
+const AboutPage = ({ title, description, keywords }: AboutPageProps) => {
   return (
     <Helmet>
       <title>Quizy - {title}</title>
       <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
     </Helmet>
   );
 };
