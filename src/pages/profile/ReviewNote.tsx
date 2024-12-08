5256fb420d2e9a034c5f558b8eec372a26f7d7f9
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-12-08 12:37:10 +0900

    feat: 오답노트 api 연결

--- src/pages/profile/ReviewNote.tsx
+++ src/pages/profile/ReviewNote.tsx
@@ -1,32 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import TopBar from '@/components/common/TopBar';
 import ReviewNoteCard from '@/components/mypage/ReviewNoteCard';
-import { useNavigate } from 'react-router-dom';
 import Container from '@/components/layout/Container';
 import FlexBox from '@/components/layout/FlexBox';
-import axiosInstance from '@/api/axiosInstance';
-import { useQuery } from '@tanstack/react-query';
-import { QuizWithAnswer } from '@/types/WrongQuizTypes';
 import { Loader2 } from 'lucide-react';
-
-const fetchIncorrectAnswers = async () => {
-  const response = await axiosInstance.get<{ result: { content: QuizWithAnswer[] } }>(
-    '/quiz/user-answers/incorrect',
-  );
-  return response.data.result.content;
-};
+import { useReviewNote } from '@/hooks/useReviewNote';
 
 export default function ReviewNote() {
+  const { incorrectQuizzes, isPending, isError, error, updateQuizReviewStatus } = useReviewNote();
   const navigate = useNavigate();
-
-  const {
-    data: incorrectQuizzes = [],
-    isPending,
-    isError,
-    error,
-  } = useQuery<QuizWithAnswer[], Error>({
-    queryKey: ['incorrectQuizzes'],
-    queryFn: fetchIncorrectAnswers,
-  });
 
   return (
     <Container>
@@ -36,7 +18,7 @@ export default function ReviewNote() {
           <Loader2 className="h-16 w-16 text-blue-500 animate-spin" />
         </div>
       ) : isError ? (
-        <div>오류가 발생했습니다: {error.message}</div>
+        <div>오류가 발생했습니다: {error!.message}</div>
       ) : incorrectQuizzes.length === 0 ? (
         <div className="flex justify-center items-center h-[calc(100vh-100px)] text-gray-500">
           틀린 문제가 없습니다
@@ -45,7 +27,7 @@ export default function ReviewNote() {
         <FlexBox direction="col" className="gap-4">
           {incorrectQuizzes.map((quizData) => (
             <div key={quizData.quiz.id} className="w-full">
-              <ReviewNoteCard quizData={quizData} />
+              <ReviewNoteCard quizData={quizData} onUpdate={updateQuizReviewStatus} />
             </div>
           ))}
         </FlexBox>
